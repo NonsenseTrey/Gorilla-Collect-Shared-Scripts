c5cd0f4eeb29c39000db4f052e2a5eb98693659b
NonsenseTrey <141064658+NonsenseTrey@users.noreply.github.com>
2025-12-17 15:19:54 -0800

    Update HandMach.cs

--- Scripts/HandMach.cs
+++ Scripts/HandMach.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HandMach : MonoBehaviour
 {
+    [Header("Script by NonsenseTrey, give credit even if edited.")]
+    [Header("Script originally shared at https://github.com/NonsenseTrey/Gorilla-Collect-Shared-Scripts")]
+    [Header("Script originally developed for the VR game on the Horizons Store Gorilla Collect.")]
+    [Header("I didn't expect sharing these scripts so all the variables will look like gibbresh.")]
     public Transform hand;
     public Transform cos;
     void Update()
@@ -12,3 +16,4 @@ public class HandMach : MonoBehaviour
         cos.rotation = hand.rotation;
     }
 }
+
